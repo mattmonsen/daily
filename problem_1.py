85e5f6044d83afb0a579ebe77b8098c9f02d839a
Matt Monsen <mdmonsen@gmail.com>
2021-01-04 16:23:32 -0700

    adding in more test cases and the question for the daily problem

--- problem_1.py
+++ problem_1.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python
 
-l = [10, 5, 7, 18];
-k = 17;
-found = False;
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+#
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
 
-while l:
-    pop = l.pop(0);
-    for i in l:
-        if (pop + i) == k:
-            found = True;
-            break;
+def sum_func(l, k):
+    print(l);
+    print(k);
+    found = False;
 
-print(found);
+    while l:
+        pop = l.pop(0);
+        for i in l:
+            if (pop + i) == k:
+                found = True;
+                break;
+    
+    return found;
+
+#l = [10, 5, 7, 18];
+#k = 17;
+
+ret = sum_func([10,5,7,18], 17);
+print(ret);
+print("");
+
+ret = sum_func([10,5,7,18], 16);
+print(ret);
+print("");
+
+ret = sum_func([10,5,7,18,75,1,15], 16);
+print(ret);
+print("");
+
+ret = sum_func([], 16);
+print(ret);
+print("");
+
+ret = sum_func([1,2,3,4,5], 9);
+print(ret);
